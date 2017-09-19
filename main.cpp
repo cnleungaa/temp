6334a201e239b03a6c3649863be3470ac19bbe0e
lcnssamlcn <samsamcheuk@gmail.com>
2017-09-19 20:17:36 +0800

    added interesting feature

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 
 
+template <typename T>
+void swap(T& x, T& y){
+    T temp = x;
+    x = y;
+    y = temp;
+}
+
 int main(){
     std::cout << "Hello World!" << std::endl;
+
+    int x = 2; int y = 10;
+    std::cout << "currently x = " << x << ", y = " << y << std::endl;
+
+    swap(x, y);
+
+    std::cout << "but now x = " << x << ", y = " << y << std::endl;
 
     return 0;
 }
